1397d7aae07a9477603924a1894b12ecc66a92cf
PiPeSiTo21 <andresfobando@unicauca.edu.co>
2026-04-21 11:57:12 -0500

    Microservicio de Citas con Spring Boot agregado

---
+++ citas-service/src/main/java/com/piedrazul/citas/CitasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.piedrazul.citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasServiceApplication.class, args);
+	}
+
+}
