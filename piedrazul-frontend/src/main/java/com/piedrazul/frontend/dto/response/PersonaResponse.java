9faaa03b552fc7d00bb5a0792f03c1b1a829fbae
RhonyMartinez <mbrhony4806@gmail.com>
2026-05-25 13:02:25 -0500

    Ajuste en listado de citas de médicos

--- piedrazul-frontend/src/main/java/com/piedrazul/frontend/dto/response/PersonaResponse.java
+++ piedrazul-frontend/src/main/java/com/piedrazul/frontend/dto/response/PersonaResponse.java
@@ -1,5 +1,8 @@
 package com.piedrazul.frontend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PersonaResponse {
 
     private Long id;
